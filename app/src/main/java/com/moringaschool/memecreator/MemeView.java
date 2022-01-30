9549be6bed1b937b8011b4622238b4947ef58025
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-30 10:06:46 +0300

    Created the meme view layout

--- app/src/main/java/com/moringaschool/memecreator/MemeView.java
+++ app/src/main/java/com/moringaschool/memecreator/MemeView.java
@@ -1,4 +1,24 @@
 package com.moringaschool.memecreator;
 
-public class MemeView {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MemeView extends AppCompatActivity {
+    @BindView(R.id.viewMemeText)
+    EditText mViewMemeText;
+    @BindView(R.id.viewSubmittedByText) EditText mViewSubmittedByText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meme_view);
+        ButterKnife.bind(this);
+
+    }
 }
