d4714911567c5df195b0d33bd34899a1692390fa
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-06 10:09:58 +0300

    Designed the API interface for the post request

--- app/src/main/java/com/moringaschool/memecreator/interfaces/ImgflipAPI.java
+++ app/src/main/java/com/moringaschool/memecreator/interfaces/ImgflipAPI.java
@@ -7,9 +7,15 @@ import com.moringaschool.memecreator.models.Meme;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ImgflipAPI {
     @GET("get_memes")
     Call<ImgflipMemeSearchResponse> getMeme();
+
+    @POST("caption_image")
+    Call<ImgflipMemeSearchResponse> postMeme( @Query( "template_id") String templateId, @Body String username, @Body String password, @Query("text0") String text0, @Query("text1") String text1);
 }
