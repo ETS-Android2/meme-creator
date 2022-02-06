ec0c073a6498e1dc480b7d60d1abf16419bdb6dc
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-06 11:09:39 +0300

    Managed to show new created meme in the createdMemeActivity file

--- app/src/main/java/com/moringaschool/memecreator/ui/CreatedMemeActivity.java
+++ app/src/main/java/com/moringaschool/memecreator/ui/CreatedMemeActivity.java
@@ -1,27 +1,32 @@
 package com.moringaschool.memecreator.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.moringaschool.memecreator.R;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CreatedMemeActivity extends AppCompatActivity {
+    @BindView(R.id.newMeme)
+    ImageView mImageView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.meme_creator);
+        setContentView(R.layout.created_meme);
         ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String newImageUrl = intent.getStringExtra("newImageUrl");
+
+        Picasso.get().load(newImageUrl).into(mImageView);
 
 
 
