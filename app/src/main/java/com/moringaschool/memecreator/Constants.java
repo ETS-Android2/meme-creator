8a398ce0738e1f4342337c1470b991155ca0d3e6
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-06 09:41:14 +0300

    Added Username and Password constants to the project

--- app/src/main/java/com/moringaschool/memecreator/Constants.java
+++ app/src/main/java/com/moringaschool/memecreator/Constants.java
@@ -3,4 +3,6 @@ package com.moringaschool.memecreator;
 public class Constants {
 
     public static final String BASE_URL = BuildConfig.BASE_URL;
+    public static final String USERNAME = BuildConfig.USERNAME;
+    public static final String PASSWORD = BuildConfig.PASSWORD;
 }
