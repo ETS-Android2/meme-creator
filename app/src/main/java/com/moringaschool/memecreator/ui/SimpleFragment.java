e27e3f936fb6a3a96708a35f43e1f4c20f4bf096
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-07 15:44:42 +0300

    Created first simple fragment that will show the title of the App

---
+++ app/src/main/java/com/moringaschool/memecreator/ui/SimpleFragment.java
@@ -0,0 +1,24 @@
+package com.moringaschool.memecreator.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.moringaschool.memecreator.R;
+
+public class SimpleFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_simple, container, false);
+        return view;
+    }
+}
