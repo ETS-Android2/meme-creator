c238ef36a224558d0bb1b078f8dff347c9c0995b
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-12 08:02:34 +0300

    Created both login activity and create account activity

---
+++ app/src/main/java/com/moringaschool/memecreator/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.memecreator.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.memecreator.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
