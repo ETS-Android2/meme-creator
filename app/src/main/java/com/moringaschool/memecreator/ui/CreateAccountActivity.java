75ecf11ea7d44c9980a0f55bc6d24696cd5e5a18
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-12 12:07:23 +0300

    Begun creating variables for my views

--- app/src/main/java/com/moringaschool/memecreator/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/memecreator/ui/CreateAccountActivity.java
@@ -3,10 +3,20 @@ package com.moringaschool.memecreator.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moringaschool.memecreator.R;
 
+import butterknife.BindView;
+
 public class CreateAccountActivity extends AppCompatActivity {
+    @BindView(R.id.newEmailEditText)
+    EditText mNewEmailEditText;
+    @BindView(R.id.newPasswordEditText) EditText mNewPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.createAccountTextView)
+    TextView mCreateAccountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
