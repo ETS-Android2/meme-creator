8cf8d7840023667dea6490ce96e839011947d1b4
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-13 14:54:43 +0300

    Created menu used for logging the user out and created a welcome display message for the user to notify them that they are logged in

--- app/src/main/java/com/moringaschool/memecreator/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/memecreator/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moringaschool.memecreator.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,9 +9,14 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.memecreator.R;
 import com.moringaschool.memecreator.clients.ImgflipClient;
 import com.moringaschool.memecreator.interfaces.ImgflipAPI;
@@ -36,17 +42,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String name;
     private String imageUrl;
 
-    ImgflipAPI imgflipAPI;
+    private ImgflipAPI imgflipAPI;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
 //        Fragment myFragment = new SimpleFragment();
 //        Bundle bundle = new Bundle();
 //        bundle.putString("imageUrl", "");
 //        myFragment.setArguments(bundle);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
 
         replaceTitleFragment(new SimpleFragment());
 
@@ -55,11 +69,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imgflipAPI = ImgflipClient.getClient();
     }
 
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    getSupportActionBar().setTitle("Welcome " + firebaseUser.getDisplayName() + "!");
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
     @Override
     public void onClick(View view ) {
         if (view == mButton) {
             myRequest();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logoutText) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 
     private void myRequest() {
