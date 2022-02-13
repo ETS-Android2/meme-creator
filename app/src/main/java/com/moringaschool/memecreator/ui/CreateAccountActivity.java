0eb86dacbb975df887f251be0489742fece2ff8b
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-02-13 08:09:25 +0300

    Created a function that authenticates users by allowing them to create new accounts

--- app/src/main/java/com/moringaschool/memecreator/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/memecreator/ui/CreateAccountActivity.java
@@ -1,16 +1,28 @@
 package com.moringaschool.memecreator.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.memecreator.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = CreateAccountActivity.class.getSimpleName();
+    @BindView(R.id.nameEditText) EditText mNameEditText;
     @BindView(R.id.newEmailEditText)
     EditText mNewEmailEditText;
     @BindView(R.id.newPasswordEditText) EditText mNewPasswordEditText;
@@ -18,9 +30,61 @@ public class CreateAccountActivity extends AppCompatActivity {
     @BindView(R.id.createAccountTextView)
     TextView mCreateAccountTextView;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+        mCreateAccountTextView.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mCreateAccountTextView) {
+            createNewUser();
+        }
+    }
+
+    private void createNewUser() {
+        String name = mNameEditText.getText().toString().trim();
+        String email = mNewEmailEditText.getText().toString().trim();
+        String password = mNewPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        if (name.equals("")) {
+            mNameEditText.setError("You cannot enter an empty string");
+            return;
+        }
+        if (email.equals("") || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mNewEmailEditText.setError("Invalid Email.");
+            return;
+        }
+        if (password.equals("") || confirmPassword.equals("")) {
+            mNewPasswordEditText.setError("Invalid Password.");
+            mConfirmPasswordEditText.setError("Invalid Password");
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(CreateAccountActivity.this, "Passwords Do Not Match!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Authentication Successful");
+                } else {
+                    Log.d(TAG, "Authentication Failed");
+                }
+            }
+        });
     }
 }
